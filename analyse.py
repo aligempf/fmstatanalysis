9be4a7f244da589217e76f37f39e55ae8ca9475e
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-01-20 22:11:27 +0000

    Make rating out of 20

--- analyse.py
+++ analyse.py
@@ -4,12 +4,22 @@ import player
 from analysisFunction import AnalysisFunction
 
 statFile = sys.argv[1]
+funFile = sys.argv[2]
+
+with open(funFile, 'r') as fin:
+    values = {}
+    reader = csv.reader(fin)
+    role = next(reader)[0]
+    for row in reader:
+        values[row[0]] = row[1]
+
+analysis = AnalysisFunction(role, values)
+print(analysis.role, ":", analysis.values)
 
 with open(statFile, 'r') as fin:
     stats = set()
-    goalkeeper = AnalysisFunction("GK", {"Aer": 1}) 
     for row in csv.DictReader(fin):
-        stats.add(player.Player(row, [goalkeeper]))
+        stats.add(player.Player(row, [analysis]))
 
 print(stats)
 print([player.values for player in stats])
--- analysisFunction.py
+++ analysisFunction.py
@@ -2,9 +2,10 @@ class AnalysisFunction:
     def __init__(self, role, values):
         self.values = values
         self.role = role
+        self.valueSum = sum(map(int, values.values()))
 
     def __call__(self, player):
         rating = 0
         for value in self.values:
-            rating += int(self.values[value]) * int(player[value])
+            rating += int(self.values[value])/self.valueSum * int(player[value])
         return rating
