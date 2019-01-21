5b9434560166b2094d0a739381292c48f72d04ae
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-01-21 16:35:55 +0000

    Rearrange analysis to not need to happen straight away

--- analyse.py
+++ analyse.py
@@ -7,7 +7,10 @@ funFiles = sys.argv[2:]
 
 analyses = [openAnalysis(funFile) for funFile in funFiles]
 
-stats = player.PlayerReaderCSV(statFile)(analyses)
+stats = player.PlayerReaderCSV(statFile)(None)
+for stat in stats:
+    for analysis in analyses:
+        stat.apply(analysis)
 
 player.PlayerWriterCSV("target/out.csv")(stats)
 
--- player.py
+++ player.py
@@ -1,5 +1,28 @@
 import csv
 
+class Player:
+    def __init__(self, playerRow, funs=None):
+        self.__dict__ = playerRow
+        self.name = playerRow["Name"]
+        if funs:
+            self.values = {fun.role: fun(self) for fun in funs}
+        else:
+            self.values = {}
+
+    def __repr__(self):
+        return "Player: " + self.name + " - " + self.Club
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def apply(self, fun):
+        self.values.update({fun.role: fun(self)})
+
+    def outputPlayer(self):
+        output = {"Name": self.Name, "Club": self.Club, "Position": self.Position}
+        output.update(self.values.items())
+        return output
+
 class PlayerReader:
     def __init__(self, statFile):
         self.statFile = statFile
@@ -14,23 +37,6 @@ class PlayerReaderCSV(PlayerReader):
             for row in csv.DictReader(fin):
                 stats.add(Player(row, analyses))
         return stats
-
-class Player:
-    def __init__(self, playerRow, funs):
-        self.__dict__ = playerRow
-        self.name = playerRow["Name"]
-        self.values = {fun.role: fun(self) for fun in funs}
-
-    def __repr__(self):
-        return "Player: " + self.name + " - " + self.Club
-
-    def __getitem__(self, key):
-        return self.__dict__[key]
-
-    def outputPlayer(self):
-        output = {"Name": self.Name, "Club": self.Club, "Position": self.Position}
-        output.update(self.values.items())
-        return output
 
 class PlayerWriter:
     def __init__(self, outFile):
