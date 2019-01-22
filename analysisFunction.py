e7b2e25f964fcac76e18e30c4c22cdababe6c263
Alistair Gempf <alistair.gempf@bbc.co.uk>
2019-01-22 08:04:11 +0000

    Map float to input analysis

--- analysisFunction.py
+++ analysisFunction.py
@@ -1,4 +1,5 @@
 import csv
+import statDefinition
 
 def openAnalysis(analysisFile):
     with open(analysisFile, 'r') as fin:
@@ -6,7 +7,10 @@ def openAnalysis(analysisFile):
         reader = csv.reader(fin)
         role = next(reader)[0]
         for row in reader:
-            values[row[0]] = row[1]
+            if row[0] in statDefinition.statDefinition:
+                values[statDefinition.statDefinition[row[0]]] = row[1]
+            else:
+                values[row[0]] = row[1]
 
     return AnalysisFunction(role, values)
 
@@ -14,10 +18,12 @@ class AnalysisFunction:
     def __init__(self, role, values):
         self.values = values
         self.role = role
-        self.valueSum = sum(map(int, values.values()))
+        self.valueSum = sum(map(float, values.values()))
 
     def __call__(self, player):
         rating = 0
         for value in self.values:
+            if player[value] == "-":
+                continue
             rating += float(self.values[value])/self.valueSum * int(player[value])
         return rating
